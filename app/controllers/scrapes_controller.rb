16b65471fa0352b10f7f118851140d01cba6b0ce
spencermeyer <spencer_meyer@hotmail.com>
2017-02-11 20:08:51 +0000

    very basic mvp

--- app/controllers/scrapes_controller.rb
+++ app/controllers/scrapes_controller.rb
@@ -1,10 +1,10 @@
 class ScrapesController < ApplicationController
-  before_action :set_scrape, only: [:show, :edit, :update, :destroy]
+  #before_action :set_scrape, only: [:show, :edit, :update, :destroy]
 
   # GET /scrapes
   # GET /scrapes.json
   def index
-    @scrapes = Scrape.all
+    #@scrapes = Scrape.all
     #SET STUFF UP
     require 'open-uri'
     online_url_for_scrape = 'http://www.parkrun.com/results/consolidatedclub/?clubNum=1537'
@@ -30,24 +30,38 @@ class ScrapesController < ApplicationController
     @links_for_scraping.each do | slink |
       run_identifier = slink[slink.index('results_')+8 .. slink.index('_parkrun_')-1 ]
       slink_doc = Nokogiri::HTML(open(slink))
+      data_set = []
       slink_doc.xpath('//tr').each do |row|
-        row.xpath('//td').each do |cell|
-          puts "cell info: #{cell.children.text}"
-        end
+        if row.children.length > 8  && row.children[0].children.text != '' && row.children[1].children.text != 'parkrunner'
+          row_data = { "pos"        => row.children[0].children.text,
+                       "parkrunner" => row.children[1].children.text,
+                       "time"       => row.children[2].children.text,
+                       "age_cat"    => row.children[3].children.text,
+                       "age_grade"  => row.children[4].children.text,
+                       "gender"     => row.children[5].children.text,
+                       "gender_pos" => row.children[6].children.text,
+                       "club"       => row.children[7].children.text,
+                       "note"       => row.children[8].children.text,
+                       "total"      => row.children[9].children.text}
+            data_set.push(row_data)
+         end 
       end
-      # binding.pry
+      # here is the place to do the sorting etc.
+      data_set.sort! {|x,y| y['age_grade'] <=> x['age_grade'] }
+      # then assign age grade position order
+      data_set.each_with_index do |data_row, index|
+        data_row['age_grade_pos']=1+index
+      end
+      # then sort back into order by position
+      data_set.sort! {|x,y| x['pos'].to_i <=> y['pos'].to_i }
 
 
-      data_set = [
-        {"parkrunner" => "Joe Blogs", "position" => "67", }, {}
-      ]
-      @data.push({run_identifier => data_set}, 'other_data' => 'like number of men etc')
+
+      @data.push({'run_identifier' => run_identifier, 'data' => data_set})
     end
 
 #  slink_doc.xpath('//tr')[0].xpath('//td')[0]
 
-    binding.pry
-    eelsfeastbuckets
 
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # resources :scrapes
-  get 'scrapes', to: 'scrapes#index'
-
   root to: 'scrapes#index'
+  get 'scrapes', to: 'scrapes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
