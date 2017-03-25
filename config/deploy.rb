a3b701de8c088e2e0348b8a744f684a8af427c7b
spencermeyer <spencer_meyer@hotmail.com>
2017-03-25 20:41:48 +0000

    added require bundler in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :log_level,     :debug
 set :keep_releases, 5
 
+
+require 'bundler/capistrano'
 # the following is a trial
 before 'deploy:assets:precompile', :symlink_config_files
 desc "Link shared files"
