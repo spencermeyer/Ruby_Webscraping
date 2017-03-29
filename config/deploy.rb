0f49a727ab870321c5d32d613229296a4ca4e42c
spencermeyer <spencer_meyer@hotmail.com>
2017-03-29 13:25:27 +0100

    changing capistrano deploy to be bitbucket

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,8 +69,10 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      #unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      unless `git rev-parse HEAD` == `git rev-parse bitbucket/master`
+        #puts "WARNING: HEAD is not the same as origin/master"
+        puts "WARNING: HEAD is not the same as bitbucket/master"
         puts "Run `git push` to sync changes."
         exit
       end
