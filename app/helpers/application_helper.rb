ebb569dc1cc9b9f93cda2519b8a231bf17dbaba4
spencermeyer <spencer_meyer@hotmail.com>
2017-05-22 20:20:39 +0100

    Added individual links using a helper method :)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def athlete_online_route(athlete_number)
+    'http://www.parkrun.org.uk/eastleigh/results/athletehistory/?athleteNumber=' + athlete_number.to_s
+  end
 end
