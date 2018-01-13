5cd3934591fbd75a24561dbb83eb3de13c2eac45
spencermeyer <spencer_meyer@hotmail.com>
2018-01-13 19:37:40 +0000

    added a map

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   root to: 'results#index'
   get 'scrapes', to: 'scrapes#index'
   get 'milestones', to: 'milestones#index'
+  get 'maps', to: 'maps#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
