6891878172608d1d1241187eb203b84753c6db7c
spencermeyer <spencer_meyer@hotmail.com>
2018-05-26 22:34:09 +0100

    make icon injector only operate locally, take out xframe options and alter nginx config

--- app/assets/javascripts/icon_injector.js
+++ app/assets/javascripts/icon_injector.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-  master = window.parent.document;
-  head = master.getElementsByTagName("head")[0];
-  favicon = master.createElement("link");
-  favicon.rel = "shortcut icon";
-  favicon.type = "image/png";
-  favicon.href = "/runner_icon2.png";
-  head.appendChild(favicon);
+  if(window.location.host.indexOf('localhost') > -1) { 
+    master = window.parent.document;
+    head = master.getElementsByTagName("head")[0];
+    favicon = master.createElement("link");
+    favicon.rel = "shortcut icon";
+    favicon.type = "image/png";
+    favicon.href = "/runner_icon2.png";
+    head.appendChild(favicon);
+  }
 });
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOW-FROM parkcollectoronrails.co.uk'
-  }
+  # config.action_dispatch.default_headers = {
+  #   'X-Frame-Options' => 'ALLOW-FROM parkcollectoronrails.co.uk'
+  # }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
