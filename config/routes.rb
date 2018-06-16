00dc1b440a4c230ec6f25b5abaed1ca06ffb3fb2
spencermeyer <spencer_meyer@hotmail.com>
2018-06-16 09:20:26 +0100

    Rescue Web View with Admin Access :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  require 'resque_web'
+
+  resque_web_constraint = lambda do |request|
+    current_user = request.env['warden'].user
+    current_user.present? && current_user.respond_to?(:admin?) && current_user.admin?
+  end
+
+  constraints resque_web_constraint do
+    mount ResqueWeb::Engine => "admin/resque_web"
+  end
+
   resources :stalkees
   devise_for :users
   resources :users
-  # resources :visits
   get 'visits', to: 'visits#index'
-  # resources :runs
-  # resources :scrapes
   resources :milestones
   resources :results
   root to: 'results#index'
