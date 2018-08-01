8ad68df65091937631b7ea8d12108c25f75d17d0
spencermeyer <spencer_meyer@hotmail.com>
2018-08-01 20:24:16 +0100

    using protect from forgery prepend true in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :set_headers
-
-  def set_headers
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"]        = "no-cache"
-    response.headers["Expires"]       = "Mon, 01 Jan 1990 00:00:00 GMT"
-  end
+  protect_from_forgery prepend: true
 end
